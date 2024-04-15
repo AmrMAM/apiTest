bc3e53489f8d71925e59408bcf627b80f93acc0a
AmrMAM <amr.mostafa.abbas@gmail.com>
2024-04-15 11:40:43 +0200

    Add project files.

---
+++ Controllers/UserController.cs
@@ -0,0 +1,28 @@
+﻿using apiTest.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.ActionConstraints;
+
+namespace apiTest.Controllers
+{
+    [Route("api/[controller]/[action]")]
+    [ApiController]
+    public class UserController : ControllerBase
+    {
+        [HttpGet]
+        public async Task<IActionResult> getUser(string uid)
+        {
+            var user = new User() {
+                age = Random.Shared.Next(20, 35),
+                city = "Cairo",
+                country = "Egypt",
+                email = $"Email_U{uid}@gmail.com",
+                name = $"user_{uid}",
+                password = $"password_{uid}",
+                phone = "+2010022003302",
+            };
+
+            return Ok( user);
+        }
+    }
+}
